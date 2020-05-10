d022e1ac6a6742409c320ec1201e72210a5168be
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-10 00:34:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,7 @@ array = [1,2,3]
 
 
 def square_array(array)
-  new_array = []
-    array.each do |numbers|
-      new_array.push(numbers ** 2)
-    end
-  return new_array
+    array.collect{|x| x ** 2 }
 end
 
 square_array(array)
